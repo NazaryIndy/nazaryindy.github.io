50ea8b4ff94b43c2a4f53d68c700c95c95454a0f
Galina Pryadkina <galya@MacBook-Pro-Galina.local>
2020-10-26 15:06:43 +0300

    Updates

---
+++ precache-manifest.7dcf79c0f40b3ada1b5a692ba7568df1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a9bc98fab47e3fa1158f01e8d0efb0",
+    "url": "/nya/index.html"
+  },
+  {
+    "revision": "c5ce4426214323816d19",
+    "url": "/nya/static/css/main.e7ef9c4e.chunk.css"
+  },
+  {
+    "revision": "6f5f825942d622c0382e",
+    "url": "/nya/static/js/2.93cfe242.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/nya/static/js/2.93cfe242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ce4426214323816d19",
+    "url": "/nya/static/js/main.885642dd.chunk.js"
+  },
+  {
+    "revision": "c76e58d21fa06b54a530",
+    "url": "/nya/static/js/runtime-main.c6d088f6.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/nya/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "d479314e3c9aa85ccbd24ef14841d9d0",
+    "url": "/nya/static/media/Pattern@2x.d479314e.png"
+  },
+  {
+    "revision": "48c4ef39806ef76eaeced814b8a5705c",
+    "url": "/nya/static/media/Trebuchet-MS.48c4ef39.ttf"
+  },
+  {
+    "revision": "6eaa7dbe80dfcd67030e7b00da9106fe",
+    "url": "/nya/static/media/cat-bg.6eaa7dbe.png"
+  },
+  {
+    "revision": "8e412cf5e5954e6134e979e7f9cb9e17",
+    "url": "/nya/static/media/pattern.8e412cf5.png"
+  }
+]);
